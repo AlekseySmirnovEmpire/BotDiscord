89819cd734e0a21d3f1b8663166f05a1bb4f0583
Aleksey Smirnov <i@smirnov-alexsey.ru>
2022-02-22 22:17:44 +0300

    true init

---
+++ RPGBot/GetToken.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace RPGBot
+{
+    internal class GetToken
+    {
+        static GetToken isCreated;
+        
+        private GetToken(){}
+
+        static public string GenerateToken()
+        {
+            if(isCreated == null)
+            {
+                isCreated = new GetToken();
+
+                var text = File.ReadAllText("config.json");
+                var json = JObject.Parse(text);
+
+                var token = json["token"].ToString();
+
+                return token;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- RPGBot/Program.cs
+++ RPGBot/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace RPGBot
 {
     internal class Program
     {
+        private DiscordSocketClient client;
+        public string token { get; private set; }
         static void Main(string[] args)
+            => new Program().MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+
+        private async Task MainAsync(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            client = new DiscordSocketClient();
+            client.Log += Log;
+
+            var token = new GetToken();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
