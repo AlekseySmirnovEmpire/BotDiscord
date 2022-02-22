00f4af84e8dec0c1a3844d2b99f9be735cc33401
Aleksey Smirnov <i@smirnov-alexsey.ru>
2022-02-22 21:28:56 +0300

    init

---
+++ RPGBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPGBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
